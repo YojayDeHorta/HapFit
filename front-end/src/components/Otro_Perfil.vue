9dbefad771492770d12dd1f2eef58718ea04a2db
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-05-28 22:19:56 -0500

    Mejoras rutinas perfil otro

--- front-end/src/components/Otro_Perfil.vue
+++ front-end/src/components/Otro_Perfil.vue
@@ -79,21 +79,7 @@
                                 </v-tab-item>
                                 <v-tab-item>
                                     <v-card flat>
-                                        2
-                                        <v-card-title class="text-h5">
-                                            An awesome title
-                                        </v-card-title>
-                                        <v-card-text>
-                                            <p>
-                                                Duis lobortis massa imperdiet quam. Donec vitae orci sed dolor rutrum auctor. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Praesent congue erat at massa.
-                                            </p>
-                                            <p>
-                                                Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Etiam sit amet orci eget eros faucibus tincidunt. Donec sodales sagittis magna.
-                                            </p>
-                                            <p class="mb-0">
-                                                Ut leo. Suspendisse potenti. Duis vel nibh at velit scelerisque suscipit. Fusce pharetra convallis urna.
-                                            </p>
-                                        </v-card-text>
+                                        <rutinas/>
                                     </v-card>
                                 </v-tab-item>
                             </v-tabs-items>
@@ -152,7 +138,13 @@
 }
 </style>
 <script>
+import rutinas from '../components/Rutinas'
+
+
 export default {
+    components: {
+        rutinas
+    },
     data() {
         return {
             dialog: false,

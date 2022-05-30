1c8d88523e6cdf2e526dc069b3f81c82cf82b748
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-05-30 13:27:19 -0500

    Admin Style

--- front-end/src/components/Dialog_perfil.vue
+++ front-end/src/components/Dialog_perfil.vue
@@ -70,29 +70,12 @@
                             <h4 style='color: #008180;'>$20.000 Mensual</h4>
                         </section><br>
                         <section class='text-center mb-5'>
-                            <v-btn style='background: #FFFFFF;border: 3px solid #E42256;box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.5);border-radius: 1rem;'>
+                            <v-btn style='background: #FFFFFF;border: 3px solid #E42256;box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.5);border-radius: 1rem;' x-large>
                                 Guardar
                             </v-btn>
                         </section>
                     </main>
                 </div>
-                <!--
-                <v-card-title class="text-h5">
-                    Use Google's location service?
-                </v-card-title>
-                <v-card-text>
-                    Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="green darken-1" text @click="dialog = false">
-                        Disagree
-                    </v-btn>
-                    <v-btn color="green darken-1" text @click="dialog = false">
-                        Agree
-                    </v-btn>
-                </v-card-actions>
-            -->
             </v-card>
         </v-dialog>
         <!--MODAL SUSCRIPCION-->
